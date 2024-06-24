6618f5175eed42ce19dd30d38e0927ae1e39afb2
ytihianine <ytihianine.pro@gmail.com>
2024-06-24 23:34:43 +0200

    feat(ui): test commit

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,16 +1,11 @@
-'use client'
+"use client";
 
 export default function Home() {
   return (
     <>
-    <div className="Content">
-    <h1>
-      HEllow worlds! 2! 3! 4! 5! 6!
-      </h1>
-    </div>
-
-
-
+      <div className="Content">
+        <h1>HEllow worlds! 2! 3! 4! 5! 6!</h1>
+      </div>
     </>
   );
 }
